23d6719e0c0384eddb5d14ebe7e20e3e8eee4a25
soonysoo <soony450@gmail.com>
2019-11-16 21:04:54 +0900

    시도중

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,27 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name: {{ name }}</div>
+    <div class="dashboard-text">namsse: {{ name }}
   </div>
+  <v-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" :size="100"></v-avatar>
+  <h1>글씨는 잘나와!</h1>
+   <el-row>
+  <el-button>Default</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
+</el-row>
+  </div>
+
+
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 
 export default {
-  name: 'Dashboard',
+  name: 'Home',
   computed: {
     ...mapGetters([
       'name'
@@ -26,5 +39,7 @@ export default {
     font-size: 30px;
     line-height: 46px;
   }
+
+
 }
 </style>
